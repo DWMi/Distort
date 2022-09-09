02fd1a0f4fa75aa8a696f26c73b6e8191448620f
David w <cf.davidwong@gmail.com>
2022-09-09 15:35:19 +0200

    final touches

--- README.md
+++ README.md
@@ -1,31 +1,44 @@
-# Distort
+# Distort 
 Chatt app made with socket.io
+
+TITEL : DISTORT 🎤
+
+
+Beskrivning:
+
+I den här inlämningen skall ni skapa en chatt-baserad app som låter användare chatta med med varandra i realtid. Kommunikationen skall sättas upp och göras med socket.io. Utöver chatt delen i uppgiften skall det även vara möjligt att skriva kommandon. Detta görs genom att börja skriva ”/” och ger då användaren förslag på giltiga kommandon - likt hur det fungerar i exempelvis Slack. API-anropet skall till den externa tjänsten skall ske via erat egna API (express server).Ni väljer själva om ni vill använda en utökad utvecklings-stack i projektet, notera att detta inte är ett krav. Exempel på ramverk ni kan lägga till i er stack är: typescript, react, mm. Låt kreativiteten flöda!
 
 
 G:
 
-1.	En fungerade chatt där man kan skriva ett kommando ”/” för att utföra en operation som integrerar med 3-part. API-anropet skall ske via er server.
+1.	En fungerade chatt där man kan skriva ett kommando ”/” för att utföra en operation som integrerar med 3-part. API-anropet skall ske via er server.✅
 
 2.	Man skall kunna sätta ett eget nickname som skickas med och visas tillsammans med meddelanden. ✅
 
-3.	Git & GitHub har använt. 
+3.	Git & GitHub har använt. ✅
 
-4.	Projektmappen innehåller en README.md fil (läs ovan för mer info).
+4.	Projektmappen innehåller en README.md fil (läs ovan för mer info).✅
 
-5.	Uppgiften lämnas in i tid. 
+5.	Uppgiften lämnas in i tid. ✅
 
 VG:
 
-6.	Alla punkter för godkänt är uppfyllda.
+6.	Alla punkter för godkänt är uppfyllda.✅
 
-7.	När någon skriver skall det synas för andra.
+7.	När någon skriver skall det synas för andra.✅
 
-8.	Det ska gå att skapa nya rum.
+8.	Det ska gå att skapa nya rum.✅
 
-9.	Alla rum som finns skall visas i en lista. Klienten skall vid klick på rum kunna gå med i rummet.
-(Jacob/David)
-10.	När man gått med i ett rum skall en lista finnas som visar alla nicknames på de som finns i det aktuella rummet.
+9.	Alla rum som finns skall visas i en lista. Klienten skall vid klick på rum kunna gå med i rummet.✅
 
-11.	Det ska finnas minst 2 kommandon (minst två olika 3-part API’er.
+10.	När man gått med i ett rum skall en lista finnas som visar alla nicknames på de som finns i det aktuella rummet.✅
+
+11.	Det ska finnas minst 2 kommandon (minst två olika 3-part API’er.✅
 
 
+Github repo: https://github.com/DWMi/Distort
+
+Instruktioner på hur projektet byggs och startas:
+
+1. npm i
+2. npm start 
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/", express.static("./client"))
 
 
 
-
+let roomData;
 io.on("connection", (socket) => { 
     console.log("Socket has connected: " + socket.id)
     io.emit("newSocketConnected", socket.id)
@@ -20,8 +20,8 @@ io.on("connection", (socket) => {
 
     socket.on("join", (socketRoomData) => {
 
-        socket.leave(socketRoomData.roomToLeave) // lämnar rum man är i om man joinar ett nytt
-        socket.join(socketRoomData.roomToJoin) // skapar ett rum genom clienten?
+        socket.leave(socketRoomData.roomToLeave) 
+        socket.join(socketRoomData.roomToJoin) 
         socket.nickname = socketRoomData.nickname
         const filteredRoomsArray = convertRoomMap()
         const roomUsers = filteredRoomsArray.find(user =>{
@@ -29,7 +29,7 @@ io.on("connection", (socket) => {
             return  user.sockets 
            } 
         })
-
+       
         const roomUserToLeave = filteredRoomsArray.find(user =>{
             if(user.room === socketRoomData.roomToLeave){
              return  user.sockets 
@@ -37,17 +37,15 @@ io.on("connection", (socket) => {
          })
   
         io.in(socketRoomData.roomToLeave).emit("getUsers",roomUserToLeave)
-        io.in(socketRoomData.roomToJoin).emit("getUsers",roomUsers)
-        // io.in(socketRoomData.roomToJoin).emit("welcome", `Välkommen ${socket.nickname}!`)
-
+        io.emit("getUsers", roomUsers, filteredRoomsArray)
+        roomData = roomUsers
     })
 
 
-    // Rums lista, körs functionen uppdateras det på alla clienter 
     socket.on("getRooms", () => {
-        console.log(io.sockets.adapter.rooms);
         const filteredRoomsArray = convertRoomMap()
         io.emit('rooms', filteredRoomsArray)
+        io.emit("getUsers", roomData, filteredRoomsArray)
     })
 
 
@@ -62,13 +60,12 @@ io.on("connection", (socket) => {
     })
 
 
-    // is typing to client
-    socket.on("isWriting", (data) => {
-        socket.broadcast.emit("isWriting", data)
-        // socket.broadcast.to(data.rooms).emit("isWriting")
+
+    socket.on("isWriting", (room) => {
+ 
+        socket.broadcast.to(room).emit("isWriting", socket.nickname)
     })
-    
-    // stop typing to client
+
     socket.on("stopWriting", () => {
         socket.broadcast.emit("stopWriting")
     })
@@ -119,25 +116,19 @@ const convertRoomMap =()=>{
 
 
     const convertedArray = Array.from(io.sockets.adapter.rooms)
-
-    // console.log(io.sockets.adapter.rooms);
-    // console.log(convertedArray);
     
     const filteredRooms = convertedArray.filter(room => ! room[1].has(room[0]))
-    // console.log(filteredRooms);
 
     const roomsWithSocketID = filteredRooms.map((roomArray) =>{
         return {room: roomArray[0], sockets:Array.from(roomArray[1])}
     })
-    // console.log(roomsWithSocketID);
-    
+
     const roomsWithIdsAndNickName = roomsWithSocketID.map((roomObj)=>{
         const nicknames = roomObj.sockets.map((socketId)=>{
             return { id: socketId, nickname: io.sockets.sockets.get(socketId).nickname }
         })
         return {room: roomObj.room, sockets: nicknames}
     })
-    // console.log(roomsWithIdsAndNickName)
 
     return roomsWithIdsAndNickName
 
@@ -145,7 +136,6 @@ const convertRoomMap =()=>{
 
 app.post('/gif', async (req, res) => {
     const { input } = req.body 
-    console.log(input)
     const gifs = await fetchGifApi(input)
     res.send(gifs)
 })
@@ -153,7 +143,6 @@ app.post('/gif', async (req, res) => {
 
 app.post('/emoji', async (req, res) => {
     const { input } = req.body 
-    console.log(input)
     const emojis = await fetchEmojiApi(input)
     res.send(emojis)
 
@@ -161,43 +150,3 @@ app.post('/emoji', async (req, res) => {
 httpServer.listen(port, () => {
     console.log("Server is running on port: " + port)
 })
-
-
-
-
-
-
-
-
-
-
-
-/*  
-socket.emit('message', "this is a test"); //sending to sender-client only. (skicka ett meddelande till den specific clienten som socketen är i.)
-
-socket.broadcast.emit('message', "this is a test"); //sending to all clients except sender
-
-socket.broadcast.to('game').emit('message', 'nice game'); //sending to all clients in 'game' room(channel) except sender
-
-socket.to('game').emit('message', 'enjoy the game'); //sending to sender client, only if they are in 'game' room(channel)
-
-socket.broadcast.to(socketid).emit('message', 'for your eyes only'); //sending to individual socketid
-
-io.emit('message', "this is a test"); //sending to all clients, include sender
-
-io.in('game').emit('message', 'cool game'); //sending to all clients in 'game' room(channel), include sender
-
-io.of('myNamespace').emit('message', 'gg'); //sending to all clients in namespace 'myNamespace', include sender
-
-socket.emit(); //send to all connected clients
-
-socket.broadcast.emit(); //send to all connected clients except the one that sent the message
-
-socket.on(); //event listener, can be called on client to execute on server
-
-io.sockets.socket(); //for emiting to specific clients
-
-io.sockets.emit(); //send to all connected clients (same as socket.emit)
-
-io.sockets.on() ; //initial connection from a client.
-*/
